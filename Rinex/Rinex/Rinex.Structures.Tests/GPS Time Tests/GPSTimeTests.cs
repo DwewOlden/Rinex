d0870fd21a1c06b55df5e0f5bc7cc836a6db982a
Andrew Olden <Andrew.Olden@outlook.com>
2017-12-15 20:20:46 +0000

    Introduced methods convert between the gps week and seconds into a DateTime object. Also introduced some testing methods

---
+++ Rinex/Rinex/Rinex.Structures.Tests/GPS Time Tests/GPSTimeTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rinex.Structures.Tests.GPS_Time_Tests
+{
+    [TestFixture]
+    public class GPSTimeTests
+    {
+        [TestCase(1980, 1, 7, 0, 0, 0, 0, 86400)]
+        [TestCase(1980, 1, 7, 0, 0, 1, 0, 86401)]
+        [TestCase(1980, 1, 7, 0, 2, 10, 0, 86530)]
+        [TestCase(1980, 1, 9, 0, 0, 0, 0, 259200)]
+        [TestCase(1986, 6, 9, 0, 0, 0, 335, 86400)]
+        [TestCase(1986, 6, 9, 18, 34, 3, 335, 153243)]
+        [TestCase(2017, 6, 6, 6, 6, 6, 1952, 194766)]
+        public void GPS_Test_2(int Year, int Month, int Date, int Hour, int Minute, int Second, int GPSWeek, int GPSSeconds)
+        {
+            IGPSTime t = new GPSTime(GPSWeek, GPSSeconds);
+
+            Assert.AreEqual(Year, t.RegularDateTime.Year);
+            Assert.AreEqual(Month, t.RegularDateTime.Month);
+            Assert.AreEqual(Date, t.RegularDateTime.Day);
+            Assert.AreEqual(Hour, t.RegularDateTime.Hour);
+            Assert.AreEqual(Minute, t.RegularDateTime.Minute);
+            Assert.AreEqual(Second, t.RegularDateTime.Second);
+        }
+
+
+        [TestCase(1980,1,7,0,0,0,0,86400)]
+        [TestCase(1980, 1, 7, 0, 0, 1, 0, 86401)]
+        [TestCase(1980, 1, 7, 0, 2, 10, 0, 86530)]
+        [TestCase(1980, 1, 9, 0, 0, 0, 0, 259200)]
+        [TestCase(1986, 6, 9, 0, 0, 0, 335, 86400)]
+        [TestCase(1986, 6, 9, 18, 34, 3, 335, 153243)]
+        [TestCase(2017, 6, 6, 6, 6, 6, 1952, 194766)]
+        public void GPS_Test_1(int Year,int Month,int Date,int Hour,int Minute,int Second,int GPSWeek,int GPSSeconds)
+        {
+            DateTime t1 = new DateTime(Year, Month, Date, Hour, Minute, Second);
+            IGPSTime t = new GPSTime(t1);
+
+            Assert.AreEqual(GPSWeek, t.GPSWeek);
+            Assert.AreEqual(GPSSeconds, t.GPSSeconds);
+        }
+    }
+}
--- Rinex/Rinex/Rinex.Structures/GPSTime.cs
+++ Rinex/Rinex/Rinex.Structures/GPSTime.cs
@@ -63,7 +63,9 @@ namespace Rinex.Structures
 
         public GPSTime(int pWeek, double pGPSSeconds)
         {
-
+            mGPSWeek_ = pWeek;
+            mSeconds_ = (int)pGPSSeconds;
+            CalculateDateTime();
         }
 
         /// <summary>
@@ -87,7 +89,7 @@ namespace Rinex.Structures
         /// <param name="pDateTime">A datetime object which will be used to populate the structure</param>
         public GPSTime(DateTime pDateTime):this(pDateTime.Year,pDateTime.Month,pDateTime.Day,pDateTime.Hour,pDateTime.Minute,(double)pDateTime.Second)
         { 
-            CalculateWeekSec();
+       
         }
 
         /// <summary>
@@ -107,9 +109,71 @@ namespace Rinex.Structures
 
             // Then calculate the number of seconds since the start of the weke
             int dayOfWeek = DaysSinceStart % RinexConstants.DaysInWeek;
-            mSeconds_ = mSeconds_ + (mDateTime_.Minute * RinexConstants.MinuteInAHour) + (dayOfWeek * RinexConstants.SecondsInDay) + (mDateTime_.Hour * RinexConstants.SecondsInHour);
+            mSeconds_ = mDateTime_.Second + (mDateTime_.Minute * RinexConstants.MinuteInAHour) + (dayOfWeek * RinexConstants.SecondsInDay) + (mDateTime_.Hour * RinexConstants.SecondsInHour);
         }
 
+        /// <summary>
+        /// Calculates the date time from the GPS week and seconds.
+        /// </summary>
+        private void CalculateDateTime()
+        {
+            int dayOfWeek;
+            int hr_, min_, sec_;
+            DateTime theDate;
 
+            GetTheDateFromGPSWeek(out dayOfWeek, out theDate);
+            CalculateTheTimeOfDay(dayOfWeek, out hr_, out min_, out sec_);
+
+            mDateTime_ = new DateTime(theDate.Year, theDate.Month, theDate.Day, hr_, min_, sec_);
+        }
+
+        /// <summary>
+        /// Calculate the time of day
+        /// </summary>
+        /// <param name="dayOfWeek">The day of the week of the GPS period</param>
+        /// <param name="hr_">The hour of the day</param>
+        /// <param name="min_">The minutes of the hour</param>
+        /// <param name="sec_">The seconds of the minute</param>
+        private void CalculateTheTimeOfDay(int dayOfWeek, out int hr_, out int min_, out int sec_)
+        {
+            float secsToday = mSeconds_ - (dayOfWeek * RinexConstants.SecondsInDay);
+            hr_ = (int)(Math.Floor(secsToday / 3600));
+            int hrsToday = (int)secsToday - (hr_ * RinexConstants.SecondsInHour);
+            min_ = (int)(Math.Floor((double)(hrsToday / 60)));
+            sec_ = (hrsToday % 60);
+        }
+
+        /// <summary>
+        /// Gets the date from the GPS weeek
+        /// </summary>
+        /// <param name="pDayOfWeek">The day of the week</param>
+        /// <param name="pDate">The date time</param>
+        private void GetTheDateFromGPSWeek(out int pDayOfWeek, out DateTime pDate)
+        {
+            int daysSinceStart = (mGPSWeek_ * RinexConstants.DaysInWeek) + 6;
+            int year_ = (int)Math.Floor(daysSinceStart / 365.25) + 1980;
+            int LeapYears = CountNumberOfYears(year_);
+
+            int daysToStartYr = ((year_ - 1980) * 365) + LeapYears;
+            int dayOfYear = daysSinceStart - daysToStartYr;
+            pDayOfWeek = (int)(Math.Floor((double)((mSeconds_ / RinexConstants.SecondsInDay))));
+            dayOfYear += pDayOfWeek;
+
+            pDate = new DateTime(year_, 1, 1).AddDays(dayOfYear - 1);
+        }
+
+        /// <summary>
+        /// Counts the number of leap years in the passed year
+        /// </summary>
+        /// <param name="year_">The current year</param>
+        /// <returns>The number of years between the passed year and 1980</returns>
+        private int CountNumberOfYears(int year_)
+        {
+            int leapyear = year_ % 4;
+            int numleaps = (int)(Math.Floor((float)(year_ - 1980) / 4.0));
+            if (leapyear != 0) numleaps++;
+
+            return numleaps;
+        }
     }
 }
