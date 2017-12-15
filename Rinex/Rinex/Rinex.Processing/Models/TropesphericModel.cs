482a2e37c7cc95d322f4ed9c78be6cc721e7b297
Andrew Olden <Andrew.Olden@outlook.com>
2017-12-15 21:24:14 +0000

    Introduced a new class used to model the tropespheric loss

---
+++ Rinex/Rinex/Rinex.Processing/Models/TropesphericModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rinex.Processing.Models
+{
+    /// <summary>
+    /// Calculates the loss due to the signal travelling througth the tropesphere
+    /// </summary>
+    /// <remarks>"Guochang Xu, GPS - Theory, Algorithms and Applications, Springer, 2007"</remarks>
+    public class TropesphericModel
+    {
+    }
+}
