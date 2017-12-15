de6abe0379af99e09988f150bd9b1f2e7f538b62
Andrew Olden <Andrew.Olden@outlook.com>
2017-12-15 21:29:23 +0000

    Some values for the standard atmosphere model.

--- Rinex/Rinex/Rinex.Processing/Models/TropesphericModel.cs
+++ Rinex/Rinex/Rinex.Processing/Models/TropesphericModel.cs
@@ -12,5 +12,19 @@ namespace Rinex.Processing.Models
     /// <remarks>"Guochang Xu, GPS - Theory, Algorithms and Applications, Springer, 2007"</remarks>
     public class TropesphericModel
     {
+        /// <summary>
+        /// Temprature taken from the standard atmosphere model
+        /// </summary>
+        private const double Temperature = 18.0;
+
+        /// <summary>
+        /// Pressure taken from the standard atmosphere model
+        /// </summary>
+        private const double Pressure = 1013.25;
+
+        /// <summary>
+        /// Humidty taken drom the standard atmosphere mode;
+        /// </summary>
+        private const double Humidity = 50.0;
     }
 }
