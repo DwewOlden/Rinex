6744d4d4d42d94b4b42a1cd6cd0895195596696a
Andrew Olden <Andrew.Olden@outlook.com>
2017-12-16 20:32:17 +0000

    Introduced the framework for the tests

---
+++ Rinex/Rinex/Rinex.Tests/Tropespheric_Model_Tests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Rinex.Processing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rinex.Tests
+{
+    [TestFixture]
+    public class Tropespheric_Model_Tests
+    {
+        private TropesphericModel model_;
+        private TroposphereModelLookups lookups_;
+
+        [Test]
+        public void Test_1()
+        {
+            lookups_ = new TroposphereModelLookups();
+
+        }
+    }
+}
